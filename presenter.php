f66361a21395de81c4055570e80daf4b684f8997
christopher.mbarapa.pro@gmail.com <test@debian>
2021-11-22 17:38:51 +0100

    commit du fichier présenter de Mbarapa par utilisateur Mbarapa

--- presenter.php
+++ presenter.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Présentation : ceci est le web de Mbarapa"
+        echo "Présentation: ceci est le web de Mbarapa"
         ?>
     </body>
 </html>
